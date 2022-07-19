c71c0ad1b84471eec2f282893c6c0b3341ba0881
Pavel Kupryianau <p.kupryianau@gmail.com>
2022-07-19 11:38:29 +0300

    modified backup script

--- backup_site.sh
+++ backup_site.sh
@@ -3,12 +3,12 @@ date=`date '+%Y%m%d%H%M%S'`
 exec >> /$HOME/backup_site_$date.log
 #TO="prkpo@mail.ru"
 
-#mkdir /$HOME/backup_site/
+sudo mkdir /$HOME/backup_site/
 #mkdir /$HOME/config
 #touch /$HOME/config/$date.txt
 #chmod 660 /$HOME/config/$date.txt
 #uname -a  >> /$HOME/config/$date.txt
-tar -czvf $date.tar.gz /var/www/site.som
+sudo tar -czvf /$HOME/backup_site/$date.tar.gz /var/www/site.som
 #mv /$HOME/*.zip /$HOME/backup/
 #echo "Result to script work" | mutt -a /$HOME/result_$date.log -s "archive" -c prkpo@mail.ru
 
